98d7ce5dc81d01dafd1937d8e97c797c064e4b41
unw10181 <uchennanwilliams@gmail.com>
2025-12-01 21:50:39 -0500

    form validation

--- script.js
+++ script.js
@@ -10,6 +10,34 @@ function isValidEmail(email) {
   return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
 }
 
+// Handle form submission, onclick done on submit btn.
+function submitEmail() {
+  const emailInput = document.getElementById("emailInput");
+  const alertBox = document.getElementById("alertBox");
+  const email = emailInput.value.trim();
+
+  // Reset styles
+  emailInput.classList.remove("error");
+  alertBox.style.display = "none";
+
+  if (!email || !isValidEmail(email)) {
+    // ERROR
+    emailInput.classList.add("error");
+    alertBox.className = "custom-alert alert-error";
+    alert("Invalid Input");
+    alertBox.textContent = "❌ Invalid email format. Please try again.";
+    alertBox.style.display = "block";
+    return;
+  }
+
+  // SUCCESS
+  alertBox.className = "custom-alert alert-success";
+  alertBox.textContent = "✅ Thank you for registering!";
+  alertBox.style.display = "block";
+
+  // clear input
+  emailInput.value = "";
+}
 
 // JS logic for dark and light mode //
 const btn = document.getElementById("theme-toggle");
